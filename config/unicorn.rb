ad5492e9f83b3b5d3afc0db1e467b25208360de9
ziraq young <ziraqyoung@outlook.com>
2021-09-01 21:54:25 +0300

    Configure unicorn and nginx

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/homw/deploy/marv"
+pid "/home/deploy/marv/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/marv/current/unicorn/unicorn.log"
+stdout_path "/home/deploy/marv/current/unicorn/unicorn.log"
+
+listen "/tmp/unicorn.marv.sock"
+worker_processes 2
+timeout 30
