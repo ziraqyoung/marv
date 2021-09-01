90914481ffda83d1268e10059b2b362afe3af7a7
ziraq young <ziraqyoung@outlook.com>
2021-09-01 22:18:31 +0300

    Unicorn confi

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,3 @@
-working_directory "/homw/deploy/marv"
 pid "/home/deploy/marv/current/tmp/pids/unicorn.pid"
 stderr_path "/home/deploy/marv/current/unicorn/unicorn.log"
 stdout_path "/home/deploy/marv/current/unicorn/unicorn.log"
