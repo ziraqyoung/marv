e5a0ee14c0563d0ce8e46656bf71cd08219ec3ee
ziraq young <ziraqyoung@outlook.com>
2021-09-03 22:43:22 +0300

    Add working_directory to unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,9 @@
+working_directory "/home/deploy/marv/current"
 pid "/home/deploy/marv/current/tmp/pids/unicorn.pid"
 stderr_path "/home/deploy/marv/current/unicorn/unicorn.log"
 stdout_path "/home/deploy/marv/current/unicorn/unicorn.log"
 
 listen "/tmp/unicorn.marv.sock"
-worker_processes 2
+worker_processes 4
 timeout 30
+preload_app true
