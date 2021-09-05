e119df6180e63169aeb60513b09fe04e2511dd4a
ziraq young <ziraqyoung@outlook.com>
2021-09-05 17:50:14 +0300

    Preserve storage folder after deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ append :linked_files, ".rbenv-vars"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, 'log', 'unicorn', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'storage', 'unicorn', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', 'node_modules'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
