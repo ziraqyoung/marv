ee9e92ed2d0111b2e3ae8af789667b396e5b9a49
ziraq young <ziraqyoung@outlook.com>
2021-09-07 23:25:46 +0300

    Fix passanger deployment error

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :branch, 'main'
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
 set :deploy_to, "/home/#{fetch :deploy_user}/#{fetch :application}"
-set :use_sudo, true
+set :passenger_restart_with_touch, true
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
